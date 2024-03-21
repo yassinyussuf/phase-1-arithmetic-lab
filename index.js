44f46ee1d33244197f33d13cd9474cf25035115b
yassinyussuf <yassinyussuf414@gmail.com>
2024-03-21 22:17:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+console.log(random);
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4;
+let numberSet = [10, 5, 20, 15];
+let max = Math.max(...numberSet);
 
 // Write your code here
